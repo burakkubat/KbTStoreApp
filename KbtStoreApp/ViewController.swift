51f347e8fe8618765addf4d72131b37b5555b86f
Burak Kubat <burakkubatdev@gmail.com>
2023-02-18 14:13:34 +0300

    Initial Commit

---
+++ KbtStoreApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KbtStoreApp
+//
+//  Created by Burak Kubat on 18.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
